e2578bd307ffb32aa8db050644ce389e4f9a0116
Dheeraj <dheerajmalvi2004@gmail.com>
2026-04-14 18:26:34 +0530

    now prices changes according to the size

--- store/models.py
+++ store/models.py
@@ -55,6 +55,7 @@ class Variation(models.Model):
     variation_value = models.CharField(max_length=100)
     is_active = models.BooleanField(default=True)
     created_date = models.DateTimeField(auto_now=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
 
     objects = VariationManager()
 
